fd33c7f5711f698e2ff217d8b4c264fb3e03d374
SleekHarpy <rtlistate@gmail.com>
2018-03-28 09:03:53 +0300

    Префиксация файлов

--- js/search_modal.js
+++ js/search_modal.js
@@ -7,24 +7,11 @@ var arrival = popup.querySelector("[name=arrival]");
 var leave = popup.querySelector("[name=leave]");
 var adults = popup.querySelector("[name=adults]");
 var children = popup.querySelector("[name=children]");
+  
 
-var isStorageSupport = true;
-var storage = "";
-  
-try {
-	storage = localStorage.getItem("adults");
-} catch (err) {
-	isStorageSupport = false;
-  }
-  
 link.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	popup.classList.toggle("search_modal_show");
-	
-	if (storage) {
-		adults.value = storage;
-	}
-	
 	arrival.focus();
   });
   
@@ -32,9 +19,5 @@ form.addEventListener("submit", function (evt) {
 	if (!arrival.value || !leave.value || !adults.value || !children.value) {
 	evt.preventDefault();
 	console.log("Нужно заполнить поля");
-	} 	else {
-		if (isStorageSupport) {
-			localStorage.setItem("adults", adults.value);
-		}
-	}
+	} 	
 });
