6721c690e6e510c66455b62ff163f241b941a15e
SleekHarpy <rtlistate@gmail.com>
2018-04-02 00:06:29 +0300

    Правки после первой проверки

--- js/search_modal.js
+++ js/search_modal.js
@@ -1,5 +1,5 @@
 var link = document.querySelector(".btn_search_hotels");
-  
+
 var popup = document.querySelector(".search_modal");
 
 var form = popup.querySelector("form");
@@ -7,17 +7,18 @@ var arrival = popup.querySelector("[name=arrival]");
 var leave = popup.querySelector("[name=leave]");
 var adults = popup.querySelector("[name=adults]");
 var children = popup.querySelector("[name=children]");
-  
 
-link.addEventListener("click", function (evt) {
-	evt.preventDefault();
-	popup.classList.toggle("search_modal_show");
-	arrival.focus();
-  });
-  
-form.addEventListener("submit", function (evt) {
-	if (!arrival.value || !leave.value || !adults.value || !children.value) {
-	evt.preventDefault();
-	console.log("Нужно заполнить поля");
-	} 	
+popup.classList.add('search_modal_hidden');
+
+link.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    popup.classList.toggle("search_modal_show");
+    arrival.focus();
+});
+
+form.addEventListener("submit", function(evt) {
+    if (!arrival.value || !leave.value || !adults.value || !children.value) {
+        evt.preventDefault();
+        console.log("Нужно заполнить поля");
+    }
 });
